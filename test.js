9966774f65117beee6777e0f7d69b8ce871f9d10
denghui.li <3379489032@qq.com>
2024-09-27 21:32:17 +0800

    change test

--- test.js
+++ test.js
@@ -4,7 +4,9 @@ import delay from './dist/index.js'
 
 test('delay 函数应在指定时间后解决', async (t) => {
   const start = Date.now()
+  console.log(start)
   await delay(1000)
   const end = Date.now()
+  console.log(end)
   t.true(end - start >= 1000)
 })
